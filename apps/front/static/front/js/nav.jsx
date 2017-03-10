33abeb1f224d7ae3e645ba6e3643dfd7f19ba24f
Scott Young <styoung89@gmail.com>
2017-03-10 16:21:33 -0600

    last

--- apps/front/static/front/js/nav.jsx
+++ apps/front/static/front/js/nav.jsx
@@ -12,26 +12,6 @@ export default class Nav extends React.Component {
     }
 
 
-    componentDidMount() {
-        $('.pour') //Pour Me Another Drink, Bartender!
-        .delay(1000)
-        .animate({
-            height: '360px'}, 1500)
-        .delay(1600)
-        .slideUp(500);
-
-        $('.liquid') // I Said Fill 'Er Up!
-        .delay(2400)
-        .animate({
-        height: '170px'
-        }, 2500);
-
-        $('.beer-foam') // Keep that Foam Rollin' Toward the Top! Yahooo!
-    .delay(2400)
-    .animate({
-      bottom: '200px'
-      }, 2500);
-    }
 
     render() {
         return (
@@ -70,7 +50,7 @@ export default class Nav extends React.Component {
 
                     </div>
                 </div>
-            </div>
+
         )
     };
 }
